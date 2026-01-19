ea56376a7af58c2e014fc2633be3dfc11903d07e
Alex <alex.barba@epn.edu.ec>
2026-01-18 21:06:16 -0500

    Primer Commit creación de la estructura del proyecto

---
+++ src/main/java/edu/epn/proyectofinal/inventarioproductos/InventarioproductosApplication.java
@@ -0,0 +1,13 @@
+package edu.epn.proyectofinal.inventarioproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioproductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioproductosApplication.class, args);
+	}
+
+}
